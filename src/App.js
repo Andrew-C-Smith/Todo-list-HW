fd3fc6c82c2aff0c327d8a3a59081d283994763a
Andrew Smith <andrew_c_smith@hotmail.com>
2022-05-09 15:24:39 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+
+  const [items, setItems] = useState([
+    { name: "Do washing", isPriority: false },
+    { name: "Go shopping", isPriority:  false },
+    { name: "Sleep", isPriority: true }
+  ])
+
+  const [newItem, setNewItem] = useState("item state")
+  
+
+
+  const itemNodes = items.map((item, index) => { 
+    return(
+        <li key={index} className={item.isComplete ? "completed" : "not-completed"}>
+          
+        <span>{item.name}</span>
+        </li> 
+    )
+})
+
+const handleItemInput = (event) =>{
+  setNewItem(event.target.value)
+}
+
+const handleItemInput2 = (event) =>{
+  setNewItem(event.target.value)
+}
+
+const saveNewItem = ((event) =>{
+  event.preventDefault()
+  const copyItem = [...items]
+  copyItem.push({name: newItem, isPriority: false})
+  setItems(copyItem)
+  setNewItem("")
+})
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h1>Todo List</h1>
+    <hr></hr>
+    <form onSubmit={saveNewItem}>
+      <label htmlFor="new-item">Add a new item:</label>   
+      <input id="new-item" type="text" onChange ={handleItemInput} value={newItem}/>  
+                  
+
+            
+      <input type="submit" value="Save New Item" />       
+    </form>
+    <ul>
+   {itemNodes}
+    </ul>
+
+
+
+  </div>
   );
 }
 
